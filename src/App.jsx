104ca66abd91b6e1e96fd6f2178b057f346c3765
Robert DeRieux <ppldiecomplex@gmail.com>
2024-10-02 16:11:24 -0600

    Add a home navbar Link component, a Route component path to the home element

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
+import { Route, Routes, Link } from "react-router-dom";
+import Red from "./components/Red";
+import Blue from "./components/Blue";
+import Home from "./components/Home";
 
+function App() {
   return (
     <>
       <div id="container">
-        <h1>Hello React Router!</h1>
-        <div id="navbar"></div>
-        <div id="main-section"></div>
+        <div id="navbar">
+          <Link to={"/"}>Home</Link>
+          <Link to={"/red"}>Red</Link>
+          <Link to={"/blue"}>Blue</Link>
+        </div>
+        <div id="main-section">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/red" element={<Red />} />
+            <Route path="/blue" element={<Blue />} />
+          </Routes>
+        </div>
       </div>
     </>
   );
